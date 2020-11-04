d10f5640484e9ae11349135c61ec31f809a67134
Robert van Kempen <DoNormal@users.noreply.github.com>
2020-11-04 21:56:27 +0100

    Add logging to remote schema server

--- src/remote-schema.ts
+++ src/remote-schema.ts
@@ -62,6 +62,28 @@ const server = new ApolloServer({
   resolvers,
   introspection: true,
   playground: true,
+  plugins: [
+    {
+      // Fires whenever a GraphQL request is received from a client.
+      requestDidStart(requestContext) {
+        console.log("Request started! Query:\n" + requestContext.request.query);
+
+        return {
+          // Fires whenever Apollo Server will parse a GraphQL
+          // request to create its associated document AST.
+          parsingDidStart(requestContext) {
+            console.log("Parsing started!");
+          },
+
+          // Fires whenever Apollo Server will validate a
+          // request's document AST against your GraphQL schema.
+          validationDidStart(requestContext) {
+            console.log("Validation started!");
+          },
+        };
+      },
+    },
+  ],
 });
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🐝 Remote schema server ready at ${url}`);
